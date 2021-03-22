02527eb88799ff8b82c0f1af9a42df144ecc1c6e
Hope93 <amal.alhaidari@gmail.com>
2021-03-22 21:31:42 +0300

    Add Local Dummy Data

---
+++ products.ts
@@ -0,0 +1,158 @@
+export type Product = {
+  id: number;
+  name: string;
+  category: {
+    name: string;
+    friendlyURL: string;
+  };
+  friendlyURL: string;
+  description: string;
+  price: number;
+  discountedPrice: number;
+  image: string;
+};
+
+const vinylProducts: Product[] = [
+  {
+    id: 1,
+    name: 'Unknown Pleasures 7"',
+    category: {
+      name: "Joy Division",
+      friendlyURL: "joy-division",
+    },
+    friendlyURL: "unknown-pleasures",
+    description: "string",
+    price: 119.99,
+    discountedPrice: 89.99,
+    image:
+      "https://merchbar.imgix.net/product/vinylized/upc/39/825646183906.jpg?w=640&h=640",
+  },
+  {
+    id: 2,
+    name: 'Turn On The Bright Lights 7"',
+    category: {
+      name: "Interpol",
+      friendlyURL: "interpol",
+    },
+    friendlyURL: "turn-on-the-bright-lights",
+    description: "string",
+    price: 119.99,
+    discountedPrice: null,
+    image:
+      "https://merchbar.imgix.net/product/vinylized/upc/45/744861054504.jpg?w=640&h=640",
+  },
+  {
+    id: 3,
+    name: 'Unknown Pleasures 7"',
+    category: {
+      name: "Joy Division",
+      friendlyURL: "joy-division",
+    },
+    friendlyURL: "unknown-pleasures",
+    description: "string",
+    price: 119.99,
+    discountedPrice: 89.99,
+    image:
+      "https://merchbar.imgix.net/product/vinylized/upc/39/825646183906.jpg?w=640&h=640",
+  },
+  {
+    id: 4,
+    name: 'Unknown Pleasures 7"',
+    category: {
+      name: "Joy Division",
+      friendlyURL: "joy-division",
+    },
+    friendlyURL: "unknown-pleasures",
+    description: "string",
+    price: 119.99,
+    discountedPrice: 89.99,
+    image:
+      "https://merchbar.imgix.net/product/vinylized/upc/39/825646183906.jpg?w=640&h=640",
+  },
+  {
+    id: 5,
+    name: 'Unknown Pleasures 7"',
+    category: {
+      name: "Joy Division",
+      friendlyURL: "joy-division",
+    },
+    friendlyURL: "unknown-pleasures",
+    description: "string",
+    price: 119.99,
+    discountedPrice: 89.99,
+    image:
+      "https://merchbar.imgix.net/product/vinylized/upc/39/825646183906.jpg?w=640&h=640",
+  },
+  {
+    id: 6,
+    name: 'Unknown Pleasures 7"',
+    category: {
+      name: "Joy Division",
+      friendlyURL: "joy-division",
+    },
+    friendlyURL: "unknown-pleasures",
+    description: "string",
+    price: 119.99,
+    discountedPrice: 89.99,
+    image:
+      "https://merchbar.imgix.net/product/vinylized/upc/39/825646183906.jpg?w=640&h=640",
+  },
+  {
+    id: 7,
+    name: 'Unknown Pleasures 7"',
+    category: {
+      name: "Joy Division",
+      friendlyURL: "joy-division",
+    },
+    friendlyURL: "unknown-pleasures",
+    description: "string",
+    price: 119.99,
+    discountedPrice: 89.99,
+    image:
+      "https://merchbar.imgix.net/product/vinylized/upc/39/825646183906.jpg?w=640&h=640",
+  },
+  {
+    id: 8,
+    name: 'Unknown Pleasures 7"',
+    category: {
+      name: "Joy Division",
+      friendlyURL: "joy-division",
+    },
+    friendlyURL: "unknown-pleasures",
+    description: "string",
+    price: 119.99,
+    discountedPrice: 89.99,
+    image:
+      "https://merchbar.imgix.net/product/vinylized/upc/39/825646183906.jpg?w=640&h=640",
+  },
+  {
+    id: 9,
+    name: 'Unknown Pleasures 7"',
+    category: {
+      name: "Joy Division",
+      friendlyURL: "joy-division",
+    },
+    friendlyURL: "unknown-pleasures",
+    description: "string",
+    price: 119.99,
+    discountedPrice: 89.99,
+    image:
+      "https://merchbar.imgix.net/product/vinylized/upc/39/825646183906.jpg?w=640&h=640",
+  },
+  {
+    id: 10,
+    name: 'Unknown Pleasures 7"',
+    category: {
+      name: "Joy Division",
+      friendlyURL: "joy-division",
+    },
+    friendlyURL: "unknown-pleasures",
+    description: "string",
+    price: 119.99,
+    discountedPrice: 89.99,
+    image:
+      "https://merchbar.imgix.net/product/vinylized/upc/39/825646183906.jpg?w=640&h=640",
+  },
+];
+
+export default vinylProducts;
