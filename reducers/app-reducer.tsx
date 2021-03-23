030357fc41b14b37fc24b87f10feebc4deedfe40
Hope93 <amal.alhaidari@gmail.com>
2021-03-23 13:59:26 +0300

    Add State Type and Reducer

---
+++ reducers/app-reducer.tsx
@@ -0,0 +1,26 @@
+import type { State } from "../states/app-state";
+import { Product } from "../products";
+
+type Action =
+  | { type: "POPULATE_PRODUCTS", products: Product[] }
+  | { type: "ADD_PRODUCT", product: Product }
+  | { type: "REMOVE_PRODUCT", id: number }
+  | { type: "REMOVE_ALL"};
+
+// Use this if managing complex state
+const appReducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "POPULATE_PRODUCTS":
+      return action.products;
+    case "ADD_PRODUCT":
+      return [...state, action.product];
+    case "REMOVE_PRODUCT":
+      return state.filter((product: Product) => product.id !== action.id);
+    case "REMOVE_ALL":
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default appReducer;
---
+++ states/app-state.tsx
@@ -0,0 +1,5 @@
+import type {Product} from '../products'
+
+export type State = Product[];
+
+export const defaultState: State = [];
